9822b02bf4b6de84c660e7cd1fa297403141e103
Durgesh <vvip202014@gmail.com>
2023-08-03 12:37:26 +0530

    V1

--- USER CMS/index.js
+++ USER CMS/index.js
@@ -11,7 +11,10 @@ const user6 = document.getElementById('usersCallBtn6');
 getUsersBtn.addEventListener('click', showUserBtn);
 
 function showUserBtn() {
-    // Fetch the data from the API using fetch or any other method
+    // Fetching the data from the API using fetch or any other method
+    const preloader = document.getElementById('preloader');
+    preloader.style.display = 'block';
+
     fetch('https://reqres.in/api/users?page=1')
         .then(response => response.json())
         .then(data => {
@@ -23,6 +26,7 @@ function showUserBtn() {
                 button.innerHTML = `${user.first_name} ${user.last_name}`;
             });
 
+            preloader.style.display = 'none';
             // Show the userBtn container
             userBtnContainer.classList.add('show');
             document.getElementById('usersCallBtn1').style.display = "block";
@@ -40,17 +44,20 @@ function showUserBtn() {
 user1.addEventListener('click', showUser1);
 
 function showUser1() {
+
+    const preloader = document.getElementById('preloader');
+    preloader.style.display = 'block';
+
     fetch('https://reqres.in/api/users?page=1')
         .then(response => response.json())
         .then(data => {
             const user = data.data[0]; // Get the first user from the API data
-
-            // Set the innerHTML of the userCard element with the user details
             userCard.innerHTML = `
                 <img src="${user.avatar}" alt="${user.first_name}">
                 <p><b>First Name:</b> ${user.first_name} <br> <b>Last Name:</b> ${user.last_name}</p>
                 <p><b>Email:</b> ${user.email}</p>
             `;
+            preloader.style.display = 'none';
             userCard.classList.add('show');
         })
         .catch(error => {
@@ -61,17 +68,20 @@ function showUser1() {
 user2.addEventListener('click', showUser2);
 
 function showUser2() {
+
+    const preloader = document.getElementById('preloader');
+    preloader.style.display = 'block';
+
     fetch('https://reqres.in/api/users?page=1')
         .then(response => response.json())
         .then(data => {
-            const user = data.data[1]; // Get the first user from the API data
-
-            // Set the innerHTML of the userCard element with the user details
+            const user = data.data[1]; // Get the second user from the API data
             userCard.innerHTML = `
                 <img src="${user.avatar}" alt="${user.first_name}">
                 <p><b>First Name:</b> ${user.first_name} <br> <b>Last Name:</b> ${user.last_name}</p>
                 <p><b>Email:</b> ${user.email}</p>
             `;
+            preloader.style.display = 'none';
             userCard.classList.add('show');
         })
         .catch(error => {
@@ -82,17 +92,20 @@ function showUser2() {
 user3.addEventListener('click', showUser3);
 
 function showUser3() {
+
+    const preloader = document.getElementById('preloader');
+    preloader.style.display = 'block';
+
     fetch('https://reqres.in/api/users?page=1')
         .then(response => response.json())
         .then(data => {
-            const user = data.data[2]; // Get the first user from the API data
-
-            // Set the innerHTML of the userCard element with the user details
+            const user = data.data[2]; // Get the third user from the API data
             userCard.innerHTML = `
                 <img src="${user.avatar}" alt="${user.first_name}">
                 <p><b>First Name:</b> ${user.first_name} <br> <b>Last Name:</b> ${user.last_name}</p>
                 <p><b>Email:</b> ${user.email}</p>
             `;
+            preloader.style.display = 'none';
             userCard.classList.add('show');
         })
         .catch(error => {
@@ -103,17 +116,20 @@ function showUser3() {
 user4.addEventListener('click', showUser4);
 
 function showUser4() {
+
+    const preloader = document.getElementById('preloader');
+    preloader.style.display = 'block';
+
     fetch('https://reqres.in/api/users?page=1')
         .then(response => response.json())
         .then(data => {
-            const user = data.data[3]; // Get the first user from the API data
-
-            // Set the innerHTML of the userCard element with the user details
+            const user = data.data[3]; // Get the fourth user from the API data
             userCard.innerHTML = `
                 <img src="${user.avatar}" alt="${user.first_name}">
                 <p><b>First Name:</b> ${user.first_name} <br> <b>Last Name:</b> ${user.last_name}</p>
                 <p><b>Email:</b> ${user.email}</p>
             `;
+            preloader.style.display = 'none';
             userCard.classList.add('show');
         })
         .catch(error => {
@@ -124,17 +140,20 @@ function showUser4() {
 user5.addEventListener('click', showUser5);
 
 function showUser5() {
+
+    const preloader = document.getElementById('preloader');
+    preloader.style.display = 'block';
+
     fetch('https://reqres.in/api/users?page=1')
         .then(response => response.json())
         .then(data => {
-            const user = data.data[4]; // Get the first user from the API data
-
-            // Set the innerHTML of the userCard element with the user details
+            const user = data.data[4]; // Get the fifth user from the API data
             userCard.innerHTML = `
                 <img src="${user.avatar}" alt="${user.first_name}">
                 <p><b>First Name:</b> ${user.first_name} <br> <b>Last Name:</b> ${user.last_name}</p>
                 <p><b>Email:</b> ${user.email}</p>
             `;
+            preloader.style.display = 'none';
             userCard.classList.add('show');
         })
         .catch(error => {
@@ -145,17 +164,20 @@ function showUser5() {
 user6.addEventListener('click', showUser6);
 
 function showUser6() {
+
+    const preloader = document.getElementById('preloader');
+    preloader.style.display = 'block';
+
     fetch('https://reqres.in/api/users?page=1')
         .then(response => response.json())
         .then(data => {
-            const user = data.data[5]; // Get the first user from the API data
-
-            // Set the innerHTML of the userCard element with the user details
+            const user = data.data[5]; // Get the sixth user from the API data
             userCard.innerHTML = `
                 <img src="${user.avatar}" alt="${user.first_name}">
                 <p><b>First Name:</b> ${user.first_name} <br> <b>Last Name:</b> ${user.last_name}</p>
                 <p><b>Email:</b> ${user.email}</p>
             `;
+            preloader.style.display = 'none';
             userCard.classList.add('show');
         })
         .catch(error => {
